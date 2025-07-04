d0db27b31ae775ea61caa6ce9c8047d68483ec14
tanishqlol <165723262+tanishqlol@users.noreply.github.com>
2025-07-04 21:47:36 +0530

    Improve model accuracy

--- feedforward-network/models/ffnn_keras.py
+++ feedforward-network/models/ffnn_keras.py
@@ -4,18 +4,18 @@ from keras import layers, callbacks
 model_simple = keras.Sequential([
 
     # hidden ReLU layer
-    layers.Dense(units=20, activation='relu', input_shape=(784,)),
-    # output layer with 10 outputs with softmax activation
-    layers.Dense(units=10, activation='softmax')
+    layers.Dense(units=128, activation='relu', input_shape=(784,)),
+    # output layer with 10 outputs with sigmoid activation
+    layers.Dense(units=10, activation='sigmoid')
 ])
 
 model_wider = keras.Sequential([
 
     # hidden ReLU layer
-    layers.Dense(units=40, activation='relu', input_shape=(784,)),
+    layers.Dense(units=256, activation='relu', input_shape=(784,)),
     layers.Dropout(rate=0.3),
-    # output layer with 10 outputs with softmax activation
-    layers.Dense(units=10, activation='softmax')
+    # output layer with 10 outputs with sigmoid activation
+    layers.Dense(units=10, activation='sigmoid')
 ])
 
 model_deeper = keras.Sequential([
@@ -32,12 +32,12 @@ model_deeper = keras.Sequential([
 model_high_capacity = keras.Sequential([
 
     # hidden ReLU layers
-    layers.Dense(units=120, activation='relu', input_shape=(784,)),
-    layers.Dropout(rate=0.2),
-    layers.Dense(units=120, activation='relu'),
+    layers.Dense(units=256, activation='relu', input_shape=(784,)),
     layers.Dropout(rate=0.4),
-    layers.Dense(units=60, activation='relu'),
-    layers.Dropout(rate=0.3),
+    layers.Dense(units=128, activation='relu'),
+    layers.Dropout(rate=0.2),
+    layers.Dense(units=128, activation='relu'),
+    layers.Dropout(rate=0.2),
     # output layer with 10 outputs with softmax activation
     layers.Dense(units=10, activation='softmax')
 ])
